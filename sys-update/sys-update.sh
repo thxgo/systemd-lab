5828d5718ee4569ca4d633a465b37a82c186a258
thxgo <paduthiago02@gmail.com>
2026-04-29 11:56:29 -0300

    feat: auto-create log file on first run

--- sys-update/sys-update.sh
+++ sys-update/sys-update.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
+[ ${EUID} -ne 0 ] && exit 1
 
-if [ ${EUID} -ne 0 ]
-then
-	exit 1 
-fi
+[ ! -f /var/log/sys-update.log ] && install -m 640 /dev/null /var/log/sys-update.log
 
-apt-get update 1>/dev/null 2>>/var/log/sys-update.log && \
+apt-get update 2>>/var/log/sys-update.log && \
 apt-get upgrade -y 2>>/var/log/sys-update.log && \
 apt-get autoremove -y 2>>/var/log/sys-update.log
 
-echo "$(date): update concluído" >> /var/log/sys-update.log
+echo "$(date): update concluído" >> /var/log/sys-update.log#!/usr/bin/env bash
